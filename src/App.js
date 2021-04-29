d25279a18fb34a1c73fde5fc36ff163d235f4109
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2021-04-29 18:05:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import logo from "./images/logoresize.jpg"
+import jean from "./images/jean gown.jpg"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header>
+
+        <img src={logo} alt="logo"/>
       </header>
+
+      <main>
+
+        <img src={jean} alt="jean"/>
+
+        <div className="content"></div>
+
+        <div>
+
+          <h2>Build a community your fans will love</h2>
+          <p>There also designs for the mobile view and active states for customers in the impementation of the designs as well</p>
+
+          <button>REGISTER</button>
+        </div>
+      </main>
+
+      <footer>
+      <i class="fab fa-facebook-f"></i> 
+      </footer>
+
+      
     </div>
   );
 }
